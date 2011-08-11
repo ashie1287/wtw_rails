4d0691434fbd9e3dfe74aef9d1d23be8b80137e9
Ashraf Hanafy <ahanafy@ash0dev.(none)>
2011-08-10 22:06:04 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WalkTheWalk::Application.config.secret_token = '7e23ec0bede6a09b0b77f39c34b4eb95db322a1c4252f853934a5c8c38f4951c6293f2423d4de22966d49453ea913b9e482bc1961f55031f22fc90047f409c4c'
