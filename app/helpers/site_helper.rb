c81821743c65da19bd2c4099434c969a78896994
Ashraf Hanafy <ahanafy@ash0dev.(none)>
2011-08-11 02:49:50 -0400

    added fonts and some style tweaks, menu partial

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,21 @@
 module SiteHelper
+  def render_menu
+    menu_hash = {
+                  'Home'        => url_for(:action => 'index'),
+=begin
+                  'Causes'      => url_for(:action => 'causes'),
+                  'Fundraising' => url_for(:action => 'fundraising'),
+                  'Sponsors'    => url_for(:action => 'sponsors'),
+                  'About'       => url_for(:action => 'about'),
+                  'Contact'     => url_for(:action => 'contact')
+=end
+                  'Causes'      => '#',
+                  'Fundraising' => '#',
+                  'Sponsors'    => '#',
+                  'About'       => '#',
+                  'Contact'     => '#'
+                }
+
+    render(:partial => 'menu', :locals => {:menu_items => menu_hash})
+  end
 end
